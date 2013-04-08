144e5d7de0d9231642694127f816c25ae9c061f2
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-04-09 00:21:58 +0900

    FontFactoryでフォントを作成するように修正した。

--- app/models/Fonts.scala
+++ app/models/Fonts.scala
@@ -1,32 +1,26 @@
 package models
 
+import com.itextpdf.awt.AsianFontMapper._
 import com.itextpdf.text._
-import com.itextpdf.text.pdf._
 
 object Fonts {
-  lazy val x = FontFactory.getFont(FontFactory.COURIER ,14, Font.BOLD, new CMYKColor(0, 255, 0, 0))
+  lazy val x = FontFactory.getFont(FontFactory.COURIER ,14, Font.BOLD, BaseColor.MAGENTA)
 
   //(ゴシック15pt(太字)
-  lazy val header =
-    new Font(BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 15, Font.BOLD)
+  lazy val header = FontFactory.getFont(JapaneseFont_Go, JapaneseEncoding_H, 15, Font.BOLD)
 
   //ゴシック11pt
-  lazy val g11 =
-    new Font(BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 11)
+  lazy val g11 = FontFactory.getFont(JapaneseFont_Go, JapaneseEncoding_H, 11)
 
   //ゴシック10pt
-  lazy val g10 =
-    new Font(BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 10)
+  lazy val g10 = FontFactory.getFont(JapaneseFont_Go, JapaneseEncoding_H, 10)
 
   //明朝10pt
-  lazy val m10 =
-    new Font(BaseFont.createFont("HeiseiMin-W3", "UniJIS-UCS2-HW-H", BaseFont.NOT_EMBEDDED), 10)
+  lazy val m10 = FontFactory.getFont(JapaneseFont_Min, JapaneseEncoding_HW_H, 10)
 
   //ゴシック11pt(下線あり)
-  lazy val underline_11 =
-    new Font(BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 11, Font.UNDERLINE)
+  lazy val underline_11 = FontFactory.getFont(JapaneseFont_Go, JapaneseEncoding_H, 11, Font.UNDERLINE)
 
   //ゴシック11pt(赤)
-  lazy val red_11 =
-    new Font(BaseFont.createFont("HeiseiKakuGo-W5", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 11, Font.NORMAL, BaseColor.RED)
+  lazy val red_11 = FontFactory.getFont(JapaneseFont_Go, JapaneseEncoding_H, 11, Font.NORMAL, BaseColor.RED)
 }
