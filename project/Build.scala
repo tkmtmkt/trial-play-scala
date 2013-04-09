1b1af208d0a8168cebce1fc4de7a3aaae6bd8173
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-04-10 07:13:07 +0900

    SLF4Jのバインディングはplay内に含まれている？

--- project/Build.scala
+++ project/Build.scala
@@ -12,8 +12,6 @@ object ApplicationBuild extends Build {
     "com.itextpdf" % "itextpdf" % "5.4.0",
     "com.itextpdf" % "itext-asian" % "5.2.0",
     "org.mongodb" %% "casbah" % "2.5.1",
-    // for logging
-    "org.slf4j" % "slf4j-simple" % "1.7.3",
     // for test
     "org.mockito" % "mockito-core" % "1.9.0" % "test",
     "org.specs2" %% "specs2" % "1.12.4" % "test",
